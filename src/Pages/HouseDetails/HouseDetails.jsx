2af8fb95a1380492495a2c8cefeac72274fc8ce2
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-10 06:50:43 +0600

    adding AOS in house Details page

--- src/Pages/HouseDetails/HouseDetails.jsx
+++ src/Pages/HouseDetails/HouseDetails.jsx
@@ -30,46 +30,74 @@ const HouseDetails = () => {
             </Helmet>
                 <div className="flex md:flex-row flex-col gap-x-6">
                     <div  
-                          className="md:w-[70%] md:mt-12">
+                          className="md:w-[70%] md:mt-12 md:px-0 px-4">
                          <img
+                         data-aos-delay="100"
+                         data-aos-duration="1000" 
+                         data-aos="fade-right"
+                         data-aos-offset="300"
+                         data-aos-easing="ease-in-sine"
                           className="md:h-[500px] h-[300px] w-full rounded-lg" src={image} alt="" />
                          <div className="grid grid-cols-3 gap-x-2 md:gap-x-5 mt-6">
                             {
                                 rooms && 
                                 rooms.map(( room ,inx)=> <img key={inx}
-                                 
+                                 data-aos="fade-up"
+                                data-aos-anchor-placement="center-bottom" 
+                                data-aos-delay="200"
+                                data-aos-duration="1000"
                                 className="w-full md:h-[200px] rounded-xl" src={room} /> ) 
                             }
                         </div>
                     </div>
                     <div className="md:w-[30%] md:px-0 px-3">
-                        <p 
+                        <p data-aos="fade-down"
+                            data-aos-delay="400"
+                            data-aos-duration="1000"
                          className="md:text-lg text-2xl md:mt-0 mt-5 font-medium text-center pb-1 border-b-2">A Luxurious {segment_name}</p>
-                        <h1 
+                        <h1 data-aos="fade-up"
+                            data-aos-delay="600"
+                            data-aos-duration="1000"
                         className="text-[#010101] text-3xl font-medium my-4">{estate_title}</h1>
-                        <p 
+                        <p data-aos="fade-up"
+                            data-aos-delay="700"
+                            data-aos-duration="1000"
                         className="text-[#6A6A6A] font-medium mb-3">Details: {description}</p>
-                        <h1 
+                        <h1 data-aos="fade-up"
+                            data-aos-delay="800"
+                            data-aos-duration="1000"
                          className="text-lg font-semibold mt-3">Facilities:</h1>
                         {
                             facilities && <ul className="list-disc space-y-2 mb-4 mt-3">
-                           <li 
+                           <li data-aos="fade-up"
+                            data-aos-delay="850"
+                            data-aos-duration="1000"
                             className="flex items-center gap-x-2 text-base bg-sky-200 px-2 py-1 rounded-lg font-medium"><FaCircleArrowRight className="text-green-600"/>{facilities[0]}</li>
-                           <li 
+                           <li data-aos="fade-up"
+                            data-aos-delay="900"
+                            data-aos-duration="1000"
                             className="flex items-center gap-x-2 text-base bg-sky-200 px-2 py-1 rounded-lg font-medium"><FaCircleArrowRight className="text-green-600"/>{facilities[1]}</li>
-                           <li 
+                           <li data-aos="fade-up"
+                            data-aos-delay="950"
+                            data-aos-duration="1000"
                             className="flex items-center gap-x-2 text-base bg-sky-200 px-2 py-1 rounded-lg font-medium"><FaCircleArrowRight className="text-green-600"/>{facilities[2]}</li>
                         </ul>
                         }
-                        <p 
+                        <p data-aos="fade-up"
+                            data-aos-delay="950"
+                            data-aos-duration="1000"
                          className="text-lg font-semibold">Area: {area}</p>
-                        <div 
+                        <div data-aos="fade-up"
+                            data-aos-delay="950"
+                            data-aos-duration="1000"
                          className="flex justify-between items-center text-lg font-semibold my-3">
                             <p>Status: For {status}</p>
                             <p>Price: {price}</p>
                         </div>
 
-                        <div 
+                        <div data-aos="fade-up"
+                            data-aos-delay="1000"
+                            data-aos-duration="1000"
                          className="">
                             <h1 className="text-lg font-semibold my-3">Location</h1>
                             <h1 className="text-2xl font-medium flex items-center gap-x-3"><MdLocationOn className="text-red-600"/>{location}</h1>
